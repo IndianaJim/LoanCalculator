7a4cf2196f5e891a22b7a42c5ecace714e78ffa6
indianajim <jimrunkel@gmail.com>
2018-09-04 20:52:49 -0400

    calcs and error check

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-alert('1');
+//listen for submit
+document
+  .getElementById('loan-form')
+  .addEventListener('submit', calculateResults);
+
+//calculate results
+function calculateResults(e) {
+  console.log('calculating...');
+  //UI vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  //compute monthly payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError('Please check your numbers');
+  }
+
+  e.preventDefault();
+}
+
+// Show error
+function showError(error) {
+  //create div
+  const errorDiv = document.createElement('div');
+
+  //get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  //add class
+  errorDiv.className = 'alert alert-danger';
+
+  //create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  //insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  //clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+//clear error
+function clearError() {
+  document.querySelector('.alert').remove();
+}
